a0fab20e03c684b59b693fcc8dd52aeefb1c945d
darshgun <darshana4it@gmail.com>
2021-01-01 23:41:42 +0530

    Format with vscode c/c++ formatter

--- src/000 blinky/sketch/sketch.ino
+++ src/000 blinky/sketch/sketch.ino
@@ -1,12 +1,17 @@
-void setup() {
+int led = 13;
+int interval = 1000;
+
+void setup()
+{
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
+  pinMode(led, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);  
+  digitalWrite(led, HIGH);
+  delay(interval);
+  digitalWrite(led, LOW);
+  delay(interval);
 }
--- src/001 debugging with serial/sketch/sketch.ino
+++ src/001 debugging with serial/sketch/sketch.ino
@@ -1,10 +1,12 @@
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Start debugging");
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   Serial.println("Test");
   delay(1000);
